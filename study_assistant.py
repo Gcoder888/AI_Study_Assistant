296b5e376881c8f6c4be8003a65d9d7ec5297f4a
Graham <gstrople8@gmail.com>
2026-08-09 16:50:19 -0700

    continuous questions

--- study_assistant.py
+++ study_assistant.py
@@ -6,11 +6,21 @@ load_dotenv()
 
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-question = input("What do you want to learn")
+first_time = True
+while(True):
+    if first_time:
+        text = "What do you want to learn: "
+        first_time = False
+    else:
+        text = "Do you have any follow up questions? \n"
+    question = input(text)
 
-reponse = client.models.generate_content(
-    model="gemini-3.6-flash", 
-    contents=f"Explain this topic to me like a beginner: {question}"
-)
+    if question == "exit":
+        break
 
-print(reponse.text)
+    reponse = client.models.generate_content(
+        model="gemini-3.6-flash", 
+        contents=f"Explain this topic to me like a beginner: {question}"
+    )
+
+    print(reponse.text)
