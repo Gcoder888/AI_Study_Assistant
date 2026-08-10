e4c87617c8782c1ecd8f1a866c0513bb698c0740
Graham <gstrople8@gmail.com>
2026-08-10 11:11:30 -0700

    system instructions introduced

--- study_assistant.py
+++ study_assistant.py
@@ -2,21 +2,35 @@ from google import genai
 from dotenv import load_dotenv
 import os
 
+# Load the variables stored in the .env file
 load_dotenv()
 
+# Create the client and the chat that will be used for the conversation
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 chat = client.chats.create(
-    model = "gemini-3.6-flash"
+    model = "gemini-3.6-flash",
+    config={
+            "system_instruction": """
+            You are a patient and encouraging Python programming tutor. 
+            
+            The user is learning Python and wants to understand
+            programming concepts rather than simple recieve answers.config=
+            
+            When teaching:
+            - Explain concepts clearly
+            - Use simple examples
+            - Explain code when necessary. 
+            - Ask the student questions to check understanding.
+            - Give practice problems when appropriate.
+            - Don't assume advanced programming knowledge. 
+            """
+        }
 )
 
-first_time = True
-while(True):
-    if first_time:
-        text = "What do you want to learn: "
-        first_time = False
-    else:
-        text = "\nYou: "
+# The loop that will run until the user tells it to stop by saying "done"
+while(True): 
+    text = "\nYou: "
     question = input(text)
 
     if question.lower() == "done":
